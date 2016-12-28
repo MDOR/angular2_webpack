55204d29e8a280ece630470032b115e1e7c79306
Marco Antonio Dominguez <madmzr@gmail.com>
2016-12-27 19:15:40 -0600

    Update webpack file. - Fix unecessary semicolon - Set a correctly rootDir - Set a correct contentBase - Update entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,22 @@
 
 const path = require('path'),
       webpack = require('webpack'),
-      HtmlWebpack = require('html-webpack-plugin');
+      HtmlWebpack = require('html-webpack-plugin'),
       ChunkWebpack = webpack.optimize.CommonsChunkPlugin;
 
-const rootDir = path.resolve(__dirname, '..');
+const rootDir = path.resolve(__dirname);
+
+console.log(rootDir)
 
 module.exports = {
   debug: true,
   devServer: {
-      contentBase: path.resolve(rootDir, 'dist'),
+      contentBase: path.resolve(rootDir, 'assets'),
       port: 9000
   },
   devtool: 'source-map',
   entry: {
-      app: [ path.resolve(rootDir, 'app', 'bootstrap') ],
-      vendor: [ path.resolve(rootDir, 'app', 'vendor') ]
+      app: [ path.resolve(rootDir, 'app', 'assets', 'js' , 'main.ts') ]
   },
   module: {
       loaders: [
@@ -29,15 +30,10 @@ module.exports = {
       path: path.resolve(rootDir, 'dist')
   },
   plugins: [
-      new ChunkWebpack({
-          filename: 'vendor.bundle.js',
-          minChunks: Infinity,
-          name: 'vendor'
-      }),
       new HtmlWebpack({
           filename: 'index.html',
           inject: 'body',
-          template: path.resolve(rootDir, 'src', 'index.html')
+          template: path.resolve(rootDir, 'app', 'index.html')
       })
   ],
   resolve: {
