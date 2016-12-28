f2b28afef44b08ab3c405e5ed87547e894f6c096
Marco Antonio Dominguez <madmzr@gmail.com>
2016-12-27 19:12:50 -0600

    Added main component, this is just an scafollding, this component will be updated.

---
+++ app/assets/js/components/main-component/main-component.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[main-component]',
+  template: ` <header>
+                Header
+              </header>
+              <main>
+                Main App
+              </main>`
+})
+
+export class MainComponent {}
